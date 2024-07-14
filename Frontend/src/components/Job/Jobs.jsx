1d094bbcc098dbcfe50fc381b2d40f6c365f3c1d
Pritam Raj <pritam271raj@gmail.com>
2024-07-14 11:19:44 +0530

    Backend URL  updated

--- Frontend/src/components/Job/Jobs.jsx
+++ Frontend/src/components/Job/Jobs.jsx
@@ -10,7 +10,7 @@ const Jobs = () => {
   useEffect(() => {
     try {
       axios
-        .get("http://localhost:4000/api/v1/job/getall", {
+        .get("https://jobzee-backend-tf2w.onrender.com/api/v1/job/getall", {
           withCredentials: true,
         })
         .then((res) => {
